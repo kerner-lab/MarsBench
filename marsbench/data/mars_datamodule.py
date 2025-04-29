644b72ab23e5a96d75be3622d3b57faa41983645
Vatsal-Malaviya <vmalaviy@asu.edu>
2025-04-29 16:05:21 -0700

    feat: persistent worker to improve i/o speed

--- marsbench/data/mars_datamodule.py
+++ marsbench/data/mars_datamodule.py
@@ -117,6 +117,8 @@ class MarsDataModule(pl.LightningDataModule):
             shuffle=True,
             num_workers=self.num_workers,
             pin_memory=True,
+            persistent_workers=True,
+            prefetch_factor=2,
             collate_fn=self.get_collate_fn(),
         )
 
